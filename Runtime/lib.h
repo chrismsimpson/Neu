e32da1b147d02f4f45e7e104c0d7cdf2ff262081
Chris Simpson <chris.m.simpson@icloud.com>
2022-06-02 15:00:29 +1000

    -

--- Runtime/lib.h
+++ Runtime/lib.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 template<typename Block>
 class Defer {
 
@@ -17,3 +19,18 @@ private:
 };
 
 void foo();
+
+using Int6 = int8_t;
+using Int16 = int16_t;
+using Int32 = int32_t;
+using Int64 = int64_t;
+using UInt8 = uint8_t;
+using UInt16 = uint16_t;
+using UInt32 = uint32_t;
+using UInt64 = uint64_t;
+
+using Float = float; // 32-bit
+using Double = double; // 64-bit
+
+static_assert(sizeof(Float) == 4);
+static_assert(sizeof(Double) == 8);
