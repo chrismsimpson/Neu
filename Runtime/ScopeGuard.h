ee74a315073a14838296dd21b317c5b287540781
Chris <chris.m.simpson@icloud.com>
2022-09-21 09:33:41 +1000

    -

---
+++ Runtime/ScopeGuard.h
@@ -0,0 +1,18 @@
+
+#pragma once 
+
+template<typename Block>
+class ScopeGuard {
+
+public:
+
+    ScopeGuard(
+        Block block)
+        : m_block(block) { }
+
+    ~ScopeGuard() { m_block(); }
+
+private:
+
+    Block m_block;
+};
--- Runtime/lib.h
+++ Runtime/lib.h
@@ -3,26 +3,9 @@
 #include <stdint.h>
 
 #include "Format.h"
+#include "ScopeGuard.h"
 
 #include "Format.cpp"
-
-template<typename Block>
-class ScopeGuard {
-
-public:
-
-    ScopeGuard(
-        Block block)
-        : m_block(block) { }
-
-    ~ScopeGuard() { m_block(); }
-
-private:
-
-    Block m_block;
-};
-
-void foo();
 
 using Int8 = int8_t;
 using Int16 = int16_t;
