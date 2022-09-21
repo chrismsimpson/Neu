4bfb7904ad8f8f511bd20a884131836d287fcd2b
Chris <chris.m.simpson@icloud.com>
2022-09-21 13:58:06 +1000

    -

--- Runtime/Format.h
+++ Runtime/Format.h
@@ -6,6 +6,15 @@
 #include "CheckedFormattedString.h"
 
 template<typename... Parameters>
+void out(
+    FILE* file, 
+    CheckedFormatString<Parameters...>&& fmtstr, 
+    Parameters const&... parameters) {
+
+    // TODO
+}
+
+template<typename... Parameters>
 void outLine(
     FILE* file, 
     CheckedFormatString<Parameters...>&& fmtstr, 
