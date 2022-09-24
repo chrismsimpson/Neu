0d9d2a63881a9a048a8d2e87d7e155645b5c7790
Chris <chris.m.simpson@icloud.com>
2022-09-24 14:40:41 +1000

    -

--- Runtime/AllOf.h
+++ Runtime/AllOf.h
@@ -2,5 +2,25 @@
 #pragma once
 
 #include "Concepts.h"
-// #include "Find.h"
+#include "Find.h"
 #include "Iterator.h"
+
+template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator>
+constexpr bool allOf(
+    TIterator const& begin,
+    TEndIterator const& end,
+    auto const& predicate) {
+
+    constexpr auto negatedPredicate = [](auto const& pred) {
+
+        return [&](auto const& elem) { return !pred(elem); };
+    };
+
+    return !(findIf(begin, end, negatedPredicate(predicate)) != end);
+}
+
+template<IterableContainer Container>
+constexpr bool allOf(Container&& container, auto const& predicate) {
+
+    return allOf(container.begin(), container.end(), predicate);
+}
--- Runtime/AnyOf.h
+++ Runtime/AnyOf.h
@@ -1,4 +1,21 @@
 
 #pragma once
 
+#include "Concepts.h"
 #include "Find.h"
+#include "Iterator.h"
+
+template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator>
+constexpr bool anyOf(
+    TIterator const& begin,
+    TEndIterator const& end,
+    auto const& predicate) {
+
+    return findIf(begin, end, predicate) != end;
+}
+
+template<IterableContainer Container>
+constexpr bool anyOf(Container&& container, auto const& predicate) {
+
+    return anyOf(container.begin(), container.end(), predicate);
+}
