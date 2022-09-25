93279e7390c96906b94272bbe41a9b6c3d4b13d0
Chris <chris.m.simpson@icloud.com>
2022-09-25 12:15:35 +1000

    -

--- Runtime/StringUtils.h
+++ Runtime/StringUtils.h
@@ -1,3 +1,11 @@
 
 #pragma once
 
+#include "Concepts.h"
+#include "Forward.h"
+
+namespace Detail {
+
+    template<Concepts::AnyString T, Concepts::AnyString U>
+    inline constexpr bool IsHashCompatible<T, U> = true;
+}
