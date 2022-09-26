318342e8629360452abb7ef9f93f9fed8a082fbf
Chris <chris.m.simpson@icloud.com>
2022-09-26 15:34:39 +1000

    -

--- Runtime/Format.cpp
+++ Runtime/Format.cpp
@@ -39,16 +39,43 @@ public:
     bool consumeReplacementField(size_t& index);
 };
 
-// template<typename... Parameters>
-// void foo(
-//     CheckedFormatString<Parameters...>&& fmtstr,
-//     Parameters const&... parameters
-// ) {
+static constexpr size_t useNextIndex = NumericLimits<size_t>::max();
 
-// }
+// The worst case is that we have the largest 64-bit value formatted as binary number, this would take
+// 65 bytes. Choosing a larger power of two won't hurt and is a bit of mitigation against out-of-bounds accesses.
 
-// void bar() {
+static constexpr size_t convert_unsigned_to_string(UInt64 value, Array<UInt8, 128>& buffer, UInt8 base, bool upper_case) {
 
-//     foo("{}", 1);
-// }
+    VERIFY(base >= 2 && base <= 16);
 
+    constexpr char const* lowercase_lookup = "0123456789abcdef";
+    constexpr char const* uppercase_lookup = "0123456789ABCDEF";
+
+    if (value == 0) {
+        buffer[0] = '0';
+        return 1;
+    }
+
+    size_t used = 0;
+    
+    while (value > 0) {
+
+        if (upper_case) {
+
+            buffer[used++] = uppercase_lookup[value % base];
+        }
+        else {
+
+            buffer[used++] = lowercase_lookup[value % base];
+        }
+
+        value /= base;
+    }
+
+    for (size_t i = 0; i < used / 2; ++i) {
+
+        swap(buffer[i], buffer[used - i - 1]);
+    }
+
+    return used;
+}
