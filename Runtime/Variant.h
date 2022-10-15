d680b85bca5e8bd4605b9cd05cd8f3c846d27ebb
Chris <chris.m.simpson@icloud.com>
2022-10-15 13:39:32 +1100

    Make variant work on windows

--- Runtime/Variant.h
+++ Runtime/Variant.h
@@ -221,8 +221,8 @@ namespace Detail {
 
             // Warning: Internal type shenanigans - VariantsConstrutors<T, Base> <- Base
             //          Not the other way around, so be _really_ careful not to cause issues.
-
-            return *reinterpret_cast<Base*>(this);
+            
+            return *static_cast<Base*>(this);
         }
     };
 
